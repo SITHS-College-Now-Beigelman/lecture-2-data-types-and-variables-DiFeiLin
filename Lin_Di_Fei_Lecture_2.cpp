2190c5a0dd9ebd376b78917eebb1000b918c11fd
DiFeiLin <difeil2@nycstudents.net>
2024-09-17 15:43:15 -0400

    completed lecture

--- Lin_Di_Fei_Lecture_2.cpp
+++ Lin_Di_Fei_Lecture_2.cpp
@@ -1,38 +1,41 @@
 //Di Fei Lin Lecture 2 9/16/24
 
 #include <iostream>
+
 #include <string>
+
 using namespace std;
 
 int main(){
-    int int_mine = 12; 
-    float float_mine = 12.12; 
-    double double_mine = 12.123;
-    char char_mine = 'H';
-    bool bool_mine = false;
-    string string_mine = "my string";
+    int intMine = 12; 
+    float floatMine = 12.12; 
+    double doubleMine = 12.123;
+    char charMine = 'H';
+    bool boolMine = false;
+    string stringMine = "my string";
 
-    cout<<"integer: "<<int_mine<<endl;
-    cout<<"float: "<<float_mine<<endl;
-    cout<<"double: "<<double_mine<<endl;
-    cout<<"character: "<<char_mine<<endl;
-    cout<<"boolean: "<<bool_mine<<endl;
-    cout<<"string: "<<string_mine<<endl;
+    cout<<"integer: "<<intMine<<endl; //naming integer
+    cout<<"float: "<<floatMine<<endl; //naming float
+    cout<<"double: "<<doubleMine<<endl; //naming double
+    cout<<"character: "<<charMine<<endl; //naming character
+    cout<<"boolean: "<<boolMine<<endl; //naming boolean
+    cout<<"string: "<<stringMine<<endl; //naming string 
 
-    char first_compare = 'A';
-    char second_compare = 'B';
-    cout<<"A - B = "<<first_compare-second_compare<<endl;
+    char firstCompare = 'A'; //establishes the char as A 
+    char secondCompare = 'B'; //establishes the char as B
+    cout<<"A < B = "<<(firstCompare<secondCompare)<<endl; //making the comparsion as true/false, returns 1 
 
     int inches;
-    inches = 100;
+    inches = 100; //establishes the integer 
 
-    cout<<inches<<" inch(es) = ";
-    cout<<inches/12<<" feet (foot) and ";
-    cout<<inches%12<<" inch(es) "<<endl;
+    cout<<inches<<" inch(es) = "; //inches prints
+    cout<<inches/12<<" feet (foot) and "; // finds the feet
+    cout<<inches%12<<" inch(es) "<<endl;//finds the remainder
 
 
-    cout<<"2 + 3.5 = "<<2 + 3.5<<endl;
-    cout<<"6 / 4 + 3.9 = "<<6 / 4 + 3.9<<endl;
-    cout<<"5.4 * 2 - (13.6 + 18 / 2) = "<<5.4 * 2 - (13.6 + 18 / 2)<<endl;
+    cout<<"2 + 3.5 = "<<2 + 3.5<<endl; //solves and prints the equation
+    cout<<"6 / 4 + 3.9 = "<<6 / 4 + 3.9<<endl; // solves and prints the equation 
+    cout<<"5.4 * 2 - (13.6 + 18 / 2) = "<<5.4 * 2 - (13.6 + 18 / 2)<<endl;// solves and prints the equation 
+    
     return 0;
 }
