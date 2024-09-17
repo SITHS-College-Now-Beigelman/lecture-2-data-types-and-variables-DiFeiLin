92679d29c25627430c18dd5bc068f07627e6750b
DiFeiLin <difeil2@nycstudents.net>
2024-09-17 15:43:25 -0400

    Update Lin_Di_Fei_Lecture_2.cpp

--- Lin_Di_Fei_Lecture_2.cpp
+++ Lin_Di_Fei_Lecture_2.cpp
@@ -7,19 +7,19 @@
 using namespace std;
 
 int main(){
-    int intMine = 12; 
-    float floatMine = 12.12; 
-    double doubleMine = 12.123;
-    char charMine = 'H';
-    bool boolMine = false;
-    string stringMine = "my string";
+    int intMine = 12; //gave 12 as the value of an int
+    float floatMine = 12.12; // gave 12.12 as as the value of a float
+    double doubleMine = 12.123; //gave 12.123 as the value of a double 
+    char charMine = 'H'; // gave H as the character value of a char 
+    bool boolMine = false; // made the bool false 
+    string stringMine = "my string"; // string named
 
-    cout<<"integer: "<<intMine<<endl; //naming integer
-    cout<<"float: "<<floatMine<<endl; //naming float
-    cout<<"double: "<<doubleMine<<endl; //naming double
-    cout<<"character: "<<charMine<<endl; //naming character
-    cout<<"boolean: "<<boolMine<<endl; //naming boolean
-    cout<<"string: "<<stringMine<<endl; //naming string 
+    cout<<"integer: "<<intMine<<endl; //print integer
+    cout<<"float: "<<floatMine<<endl; //print float
+    cout<<"double: "<<doubleMine<<endl; //print double
+    cout<<"character: "<<charMine<<endl; //print character
+    cout<<"boolean: "<<boolMine<<endl; //print boolean
+    cout<<"string: "<<stringMine<<endl; //print string 
 
     char firstCompare = 'A'; //establishes the char as A 
     char secondCompare = 'B'; //establishes the char as B
@@ -36,6 +36,6 @@ int main(){
     cout<<"2 + 3.5 = "<<2 + 3.5<<endl; //solves and prints the equation
     cout<<"6 / 4 + 3.9 = "<<6 / 4 + 3.9<<endl; // solves and prints the equation 
     cout<<"5.4 * 2 - (13.6 + 18 / 2) = "<<5.4 * 2 - (13.6 + 18 / 2)<<endl;// solves and prints the equation 
-    
+
     return 0;
 }
